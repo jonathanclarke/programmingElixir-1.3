1dad2850c9e04fee545666f39213292dc0b6f09d
Jonathan Clarke <jonathan@vayu.com.au>
2017-04-18 18:50:53 +0545

    Chapter Four - with statement and pattern matching

--- lib/chapterFour/WithScope.ex
+++ lib/chapterFour/WithScope.ex
@@ -28,4 +28,24 @@ defmodule WithScope do
   end
 
 
+  
+
+  @doc """
+  example
+  ## Examples
+
+      iex> WithScope.example_with_nil
+      nil
+  """
+  def example_with_nil do
+    _result = with {:ok, file} = File.open("/etc/passwd"),
+                  content       = IO.read(file, :all),
+                    :ok           = File.close(file),
+                    [_, uid, gid] <- Regex.run(~r/XXX:.*?:(\d+):(\d+)/, content) do
+               
+               "Group: #{ gid}, User: #{uid}"
+             end
+  end
+
+
 end
--- test/chapterFour/WithScope_test.exs
+++ test/chapterFour/WithScope_test.exs
@@ -5,5 +5,17 @@ defmodule WithScopeTest do
   test "example of the with expression" do
     assert WithScope.example == "Now is the time"
   end
+
   
+  test "with pattern matching" do
+    eg_one = with [a|_] <- [1,2,3], do: a
+    eg_two = with [a|_] <- nil, do: a
+
+    assert eg_one == 1
+    assert eg_two == nil
+  end
+  
+  test "using pattern matching but raise nil instead of an exception using <- instead of =" do
+    assert WithScope.example_with_nil == nil
+  end
 end
